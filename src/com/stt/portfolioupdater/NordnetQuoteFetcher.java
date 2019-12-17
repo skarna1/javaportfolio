19fda01a2c65f29c0c5ed9f1c2fd9441621e38ec
Sami K <sami.karna@gmail.com>
2019-12-17 18:28:17 +0200

    fix

--- src/com/stt/portfolioupdater/NordnetQuoteFetcher.java
+++ src/com/stt/portfolioupdater/NordnetQuoteFetcher.java
@@ -14,10 +14,10 @@ import javax.xml.xpath.XPathExpressionException;
 
 
 public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
-	
+
 	protected Map<String, String> tickers;
 	protected String SYMBOL_MAP_FILE = "etc/NordnetStockQuoteFetcher.txt";
-	
+
 	public NordnetQuoteFetcher() {
 		tickers = new HashMap<>();
 		readTickers(this.SYMBOL_MAP_FILE);
@@ -29,14 +29,14 @@ public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
 		tickers = new HashMap<>();
 		readTickers(this.SYMBOL_MAP_FILE);
 	}
-	
+
 	@Override
 	public List<Item> parseHtml() {
-	
+
 		//System.out.println(getUri());
 		//System.out.println(getXpath());
-		
-		
+
+
 		List<Item> items = new ArrayList<>();
 
 		try {
@@ -51,10 +51,10 @@ public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
 					parseRow(item, tr);
 					item.setGivenDate(new Date());
 					//item.print();
-					
+
 					if (item.getTicker() != null) {
 						items.add(item);
-						item.print();
+						//item.print();
 					}
 				}
 			}
@@ -66,7 +66,7 @@ public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
 		return items;
 	}
 
-	
+
 	private void parseRow(Item item, org.w3c.dom.Node tr) {
 		org.w3c.dom.NodeList nodes = tr.getChildNodes();
 		//System.out.println("Columns : " + nodes.getLength());
@@ -80,76 +80,86 @@ public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
 		//org.w3c.dom.NodeList nodes = td.getChildNodes();
 		//for (int i = 0; i < nodes.getLength(); ++i) {
 		//	org.w3c.dom.Node tdNode = nodes.item(i);
-		
-	
+
+
 		org.w3c.dom.Node tdNode = td.getFirstChild();
-			if (column == 4) {				
-				if (tdNode != null && tdNode.getNodeName().equalsIgnoreCase("a")) {
-					org.w3c.dom.Node n = tdNode.getFirstChild();
-					String name = n.getNodeValue();
-					item.setName(name);
-					System.out.println(name + " " + getTicker(name));
-					item.setTicker(getTicker(name));
-				
-				}
-			} else if (column == 7) {
-				// div, span, span, span, span /span, /span, span,span, /span, /span, span value /span, span, /span,/span,/span/div
-				// last
-				//tdNode = tdNode.getFirstChild();  // div
-				if (tdNode != null) tdNode = tdNode.getFirstChild(); // span
-				
-				org.w3c.dom.NodeList childs = tdNode.getChildNodes();
-				if (tdNode != null) tdNode = childs.item(2);
-				
+		if (column == 4) {				
+			if (tdNode != null && tdNode.getNodeName().equalsIgnoreCase("a")) {
+				org.w3c.dom.Node n = tdNode.getFirstChild();
+				String name = n.getNodeValue();
+				item.setName(name);
+				//System.out.println(name + " " + getTicker(name));
+				item.setTicker(getTicker(name));
+
+			}
+		} else if (column == 7) {
+			// div, span, span, span, span /span, /span, span,span, /span, /span, span value /span, span, /span,/span,/span/div
+			// last
+			//tdNode = tdNode.getFirstChild();  // div
+			if (tdNode != null) tdNode = tdNode.getFirstChild(); // span
+
+			org.w3c.dom.NodeList childs = tdNode.getChildNodes();
+			if (tdNode != null) tdNode = childs.item(2);
+
+			String columnValue = this.getValue(tdNode);
+			//System.out.println("Column " + column + " value: " + columnValue);
+			try {
+				double v = Double.parseDouble(columnValue);
+				item.setLast(v);
+			}
+			catch (NumberFormatException e ) {
+
+			}
+		}
+		else if (column == 11) {
+			// high
+
+			if (tdNode != null) {
+
 				String columnValue = this.getValue(tdNode);
-				System.out.println("Column " + column + " value: " + columnValue);
+				//System.out.println("Column " + column + " value: " + columnValue);
 				try {
 					double v = Double.parseDouble(columnValue);
-					item.setLast(v);
+					item.setHigh(v);
 				}
 				catch (NumberFormatException e ) {
-					
-				}
-			}
-			else if (column == 11) {
-				// high
-				
-				if (tdNode != null) {
-				
-				String columnValue = this.getValue(tdNode);
-				//System.out.println("Column " + column + " value: " + columnValue);
-				double v = Double.parseDouble(columnValue);
-				item.setHigh(v);
-				}
-			}
-			else if (column == 12) {
-				// low
 
-				if (tdNode != null) {
-			
-				String columnValue = this.getValue(tdNode);
-				//System.out.println("Column " + column + " value: " + columnValue);
-				double v = Double.parseDouble(columnValue);
-				item.setLow(v);
 				}
 			}
+		}
+		else if (column == 12) {
+			// low
+
+			if (tdNode != null) {
+
+				String columnValue = this.getValue(tdNode);
+				//System.out.println("Column " + column + " value: " + columnValue);
+				try {
+					double v = Double.parseDouble(columnValue);
+					item.setLow(v);
+				}
+				catch (NumberFormatException e ) {
+
+				}
+			}
+		}
 		//}
 	}
-	
+
 	private String getValue(org.w3c.dom.Node node) {
 		if (node == null) return "";
 		String value = node.getNodeValue().trim();
 		value = value.replace(',', '.');
 		value = value.replaceAll(" ", "");
 		value = value.replaceAll("[^\\d.]", "");
-		
+
 		return value;
 	}
 
 	protected String getTicker(String name) {
 		//System.out.println(name);
 		if (this.tickers.containsKey(name)) {
-			
+
 			return this.tickers.get(name);
 		}
 		else {
@@ -157,9 +167,9 @@ public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
 		}
 		return null;
 	}
-	
+
 	public void readTickers(String filename) {
-		
+
 		BufferedReader br = null;
 		try {
 			br = new BufferedReader(new FileReader(filename));
@@ -175,31 +185,31 @@ public class NordnetQuoteFetcher extends HTTPQuoteFetcher {
 						tickers.put(name, ticker);
 					}
 				}
-	            line = br.readLine();
-	        }
+				line = br.readLine();
+			}
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		catch (IOException e) {
 		}
-	    
-	    finally {
-	    	if (br != null) {
-	    		try {
+
+		finally {
+			if (br != null) {
+				try {
 					br.close();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-	    	}
-	    }
+			}
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		String uri = "https://www.nordnet.fi/markkinakatsaus/osakekurssit/";
+		String uri = "https://www.nordnet.fi/markkinakatsaus/osakekurssit/?page=2&exchangeCountry=FI";
 		String xpathstr = "//table[contains(@class, ' md')]/tbody/tr";
-		
+
 		NordnetQuoteFetcher fetcher = new NordnetQuoteFetcher(uri, 
 				xpathstr);
 		List<Item> items = fetcher.parseHtml();
