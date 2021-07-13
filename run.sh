98785472eb498a247a4cc71270c78d1ccb2a61b7
Sami K <sami.karna@gmail.com>
2021-07-13 19:17:56 +0300

    Fix classpath

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 cd /home/sami/workspace/javaportfolio
 
-java -Xmx1024m -cp "bin:target/lib/*"  com.stt.portfolio.PortfolioGuiApp
+java -Xmx1024m -cp "target/classes:target/lib/*"  com.stt.portfolio.PortfolioGuiApp
