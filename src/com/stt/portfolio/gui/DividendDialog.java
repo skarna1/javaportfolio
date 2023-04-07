9555be2a8fbc3dc36205de6131215a1fcbf1e5eb
Sami K <sami.karna@gmail.com>
2023-04-07 23:20:52 +0300

    Add dividend tax calculation

--- src/com/stt/portfolio/gui/DividendDialog.java
+++ src/com/stt/portfolio/gui/DividendDialog.java
@@ -9,9 +9,14 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.swing.Box;
+import javax.swing.ButtonGroup;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 import com.stt.portfolio.BookEntry;
 import com.stt.portfolio.I_TickerManager;
@@ -22,12 +27,19 @@ public class DividendDialog extends BaseDialog {
 	private static String SECTOR_SELECTED = "sector";
 	private static String STOCK_SELECTED = "stock";
 	private static String BROKER_SELECTED = "broker";
+	private static String TAX_SELECTED = "tax";
 
 	private JTextField totalCostField = new JTextField(FIELD_LEN);
 	private JTextField netCostField = new JTextField(FIELD_LEN);
 	private JTextField amountField = new JTextField(FIELD_LEN);
 	private JTextField costField = new JTextField(FIELD_LEN);
 	private JTextField dividendTaxField = new JTextField(FIELD_LEN);
+
+	private ButtonGroup taxButtons = new ButtonGroup();
+	Box buttonBox;
+	JRadioButton tax0 = new JRadioButton("0 %");
+	JRadioButton tax15 = new JRadioButton("15 %");
+	JRadioButton tax255 = new JRadioButton("25,5%");
 
 	JComboBox<String> brokerList;
 	JComboBox<String> sectorList;
@@ -44,11 +56,29 @@ public class DividendDialog extends BaseDialog {
 	JLabel dividendTaxLabel;
 	private JLabel localCurrencyButtonLabel;
 
-
-
 	private Map<String, List<String>> stocks = null;
 	private I_TickerManager tickerManager = null;
 	private Map<String, Collection<BookEntry>> bookEntries;
+
+	@FunctionalInterface
+	public interface SimpleDocumentListener extends DocumentListener {
+		void update(@SuppressWarnings("exports") DocumentEvent e);
+
+		@Override
+		default void insertUpdate(@SuppressWarnings("exports") DocumentEvent e) {
+			update(e);
+		}
+
+		@Override
+		default void removeUpdate(@SuppressWarnings("exports") DocumentEvent e) {
+			update(e);
+		}
+
+		@Override
+		default void changedUpdate(@SuppressWarnings("exports") DocumentEvent e) {
+			update(e);
+		}
+	}
 
 	public String getStockName() {
 		return String.valueOf(stocksList.getSelectedItem());
@@ -92,11 +122,8 @@ public class DividendDialog extends BaseDialog {
 			updateForeignCurrency(stockName);
 			updateOwnedAmount(broker, stockName);
 		}
-		else if (CURRENCY_SELECTED.equals(e.getActionCommand())) {
-			Object item = currencyList.getSelectedItem();
-			if (item != null) {
-				updateRateFieldCcy((String) item);
-			}
+		else if (TAX_SELECTED.equals(e.getActionCommand())) {
+			updateTax();
 		}
 		else {
 			super.actionPerformed(e);
@@ -149,9 +176,33 @@ public class DividendDialog extends BaseDialog {
 		stocksList.setActionCommand(STOCK_SELECTED);
 		stocksList.addActionListener(this);
 
+		// tax percents
+		taxButtons.add(tax0);
+		taxButtons.add(tax15);
+		taxButtons.add(tax255);
+		buttonBox = Box.createHorizontalBox();
+		buttonBox.add(tax0);
+		buttonBox.add(tax15);
+		buttonBox.add(tax255);
+		tax255.setSelected(true);
+		tax0.setActionCommand(TAX_SELECTED);
+		tax15.setActionCommand(TAX_SELECTED);
+		tax255.setActionCommand(TAX_SELECTED);
+		tax0.addActionListener(this);
+		tax15.addActionListener(this);
+		tax255.addActionListener(this);
+
 		amountField.addKeyListener(this);
 		costField.addKeyListener(this);
 		dividendTaxField.addKeyListener(this);
+
+		amountField.getDocument().addDocumentListener((SimpleDocumentListener) e -> {
+			updateTax();
+		});
+
+		costField.getDocument().addDocumentListener((SimpleDocumentListener) e -> {
+			updateTax();
+		});
 
 		totalCostField.setEditable(false);
 		totalCostField.setText("0.00");
@@ -262,16 +313,33 @@ public class DividendDialog extends BaseDialog {
 		netCostField.setText(String.format("%1$.2f", netCost));
 	}
 
+	private void updateTax() {
+		double taxPercent = 25.5;
+		if (tax0.isSelected()) {
+			taxPercent = 0.0;
+		} else if (tax15.isSelected()) {
+			taxPercent = 15.0;
+		} else if (tax255.isSelected()) {
+			taxPercent = 25.5;
+		}
+		double amount = convertToDouble(amountField.getText());
+		double cost = convertToDouble(costField.getText());
+		double totalCost = amount * cost;
+		double tax = totalCost * taxPercent / 100.0;
+		dividendTaxField.setText(String.format("%1$.2f", tax));
+	}
+
 	protected Component[] getDialogComponents() {
 		Component[] components = { sectorList, stocksList, dateChooser, currencyList, amountField, costField, rateField,
-				brokerList, dividendTaxField, totalCostField, netCostField };
+				brokerList, buttonBox, dividendTaxField, totalCostField, netCostField };
 		return components;
 	}
 
 	protected JLabel[] getDialogLabels() {
 		JLabel[] labels = { sectorFieldLabel, stockFieldLabel, dateFieldLabel, localCurrencyButtonLabel,
 				amountFieldLabel, costFieldLabel, rateFieldLabel,
-				brokerFieldLabel, dividendTaxLabel, totalCostFieldLabel, netCostFieldLabel };
+				brokerFieldLabel, new JLabel("tax percents"),
+				totalCostFieldLabel, netCostFieldLabel };
 		return labels;
 	}
 
